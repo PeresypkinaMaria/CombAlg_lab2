b449031a98c5c61c5278c3c3d627b05d42f7bd8d
Мария Пересыпкина <peresypkina555@gmail.com>
2018-12-08 01:15:34 +0300

    забыла закоммитить все

--- CombAlg_lab2/Form1.Designer.cs
+++ CombAlg_lab2/Form1.Designer.cs
@@ -28,13 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblDesertSize = new System.Windows.Forms.Label();
+            this.tbDesertSize = new System.Windows.Forms.TextBox();
+            this.btnTask = new System.Windows.Forms.Button();
+            this.lblTankSize = new System.Windows.Forms.Label();
+            this.tbTankSize = new System.Windows.Forms.TextBox();
+            this.lblCanisterSize = new System.Windows.Forms.Label();
+            this.tbCanisterSize = new System.Windows.Forms.TextBox();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblDesertSize
+            // 
+            this.lblDesertSize.AutoSize = true;
+            this.lblDesertSize.Location = new System.Drawing.Point(12, 105);
+            this.lblDesertSize.Name = "lblDesertSize";
+            this.lblDesertSize.Size = new System.Drawing.Size(116, 17);
+            this.lblDesertSize.TabIndex = 0;
+            this.lblDesertSize.Text = "Размер пустыни";
+            // 
+            // tbDesertSize
+            // 
+            this.tbDesertSize.Location = new System.Drawing.Point(15, 125);
+            this.tbDesertSize.Name = "tbDesertSize";
+            this.tbDesertSize.Size = new System.Drawing.Size(79, 22);
+            this.tbDesertSize.TabIndex = 1;
+            // 
+            // btnTask
+            // 
+            this.btnTask.Location = new System.Drawing.Point(15, 45);
+            this.btnTask.Name = "btnTask";
+            this.btnTask.Size = new System.Drawing.Size(79, 30);
+            this.btnTask.TabIndex = 2;
+            this.btnTask.Text = "Задание";
+            this.btnTask.UseVisualStyleBackColor = true;
+            this.btnTask.Click += new System.EventHandler(this.btnTask_Click);
+            // 
+            // lblTankSize
+            // 
+            this.lblTankSize.AutoSize = true;
+            this.lblTankSize.Location = new System.Drawing.Point(12, 182);
+            this.lblTankSize.Name = "lblTankSize";
+            this.lblTankSize.Size = new System.Drawing.Size(92, 17);
+            this.lblTankSize.TabIndex = 3;
+            this.lblTankSize.Text = "Размер бака";
+            // 
+            // tbTankSize
+            // 
+            this.tbTankSize.Location = new System.Drawing.Point(12, 202);
+            this.tbTankSize.Name = "tbTankSize";
+            this.tbTankSize.Size = new System.Drawing.Size(82, 22);
+            this.tbTankSize.TabIndex = 4;
+            // 
+            // lblCanisterSize
+            // 
+            this.lblCanisterSize.AutoSize = true;
+            this.lblCanisterSize.Location = new System.Drawing.Point(12, 263);
+            this.lblCanisterSize.Name = "lblCanisterSize";
+            this.lblCanisterSize.Size = new System.Drawing.Size(124, 17);
+            this.lblCanisterSize.TabIndex = 5;
+            this.lblCanisterSize.Text = "Размер канистры";
+            // 
+            // tbCanisterSize
+            // 
+            this.tbCanisterSize.Location = new System.Drawing.Point(15, 283);
+            this.tbCanisterSize.Name = "tbCanisterSize";
+            this.tbCanisterSize.Size = new System.Drawing.Size(79, 22);
+            this.tbCanisterSize.TabIndex = 6;
+            // 
+            // tbResult
+            // 
+            this.tbResult.Location = new System.Drawing.Point(175, 45);
+            this.tbResult.Multiline = true;
+            this.tbResult.Name = "tbResult";
+            this.tbResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbResult.Size = new System.Drawing.Size(517, 260);
+            this.tbResult.TabIndex = 7;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(5, 330);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(131, 30);
+            this.btnRun.TabIndex = 8;
+            this.btnRun.Text = "Найти решение";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(598, 330);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(94, 30);
+            this.btnClear.TabIndex = 9;
+            this.btnClear.Text = "Очистить";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(757, 383);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.tbCanisterSize);
+            this.Controls.Add(this.lblCanisterSize);
+            this.Controls.Add(this.tbTankSize);
+            this.Controls.Add(this.lblTankSize);
+            this.Controls.Add(this.btnTask);
+            this.Controls.Add(this.tbDesertSize);
+            this.Controls.Add(this.lblDesertSize);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblDesertSize;
+        private System.Windows.Forms.TextBox tbDesertSize;
+        private System.Windows.Forms.Button btnTask;
+        private System.Windows.Forms.Label lblTankSize;
+        private System.Windows.Forms.TextBox tbTankSize;
+        private System.Windows.Forms.Label lblCanisterSize;
+        private System.Windows.Forms.TextBox tbCanisterSize;
+        private System.Windows.Forms.TextBox tbResult;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnClear;
     }
 }
 
--- CombAlg_lab2/Form1.cs
+++ CombAlg_lab2/Form1.cs
@@ -12,9 +12,107 @@ namespace CombAlg_lab2
 {
     public partial class Form1 : Form
     {
+        double N, M, V; //размер пустыни, бака и канистры
+
+        string overcome = "Машина проехала расстояние ";
+        string leave_canister = " и оставила канистру";
+        //string take_canister = "Машина взяла оставленную канистру";
+        string return_to_base = "Машина вернулась на базу";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnTask_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("18. «Переезд пустыни». Дана пустыня – N км. В начале располагается автомобиль с баком – M л," +
+                "который может перевозить с собой 2 канистры - по V (N > M > V) л каждая. Считая, что в начале пути имеется" +
+                "достаточное количество топлива, необходимо определить кол-во используемого топлива и последовательность" +
+                "передвижения автомобиля по пустыни для пересечения пустыни с минимальными затратами. Расход топлива считать равным 1л/км." +
+                "Подсказка: использовать метод динамического программирования и рассмотреть задачу с финиша.");
+        }
+
+        //проверка ввода чисел
+        private bool CheckVal()
+        {
+            if (!Double.TryParse(tbDesertSize.Text, out N))
+            {
+                MessageBox.Show("Введите число в поле 'Размер пустыни'!");
+                return false;
+            }
+            if (!Double.TryParse(tbTankSize.Text, out M))
+            {
+                MessageBox.Show("Введите число в поле 'Размер бака'!");
+                return false;
+            }
+            if (!Double.TryParse(tbCanisterSize.Text, out V))
+            {
+                MessageBox.Show("Введите число в поле 'Размер канистры'!");
+                return false;
+            }
+            return true;
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            tbResult.Text = "";
+            Task();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            tbDesertSize.Text = "";
+            tbTankSize.Text = "";
+            tbCanisterSize.Text = "";
+            tbResult.Text = "";
+        }
+
+        //проверка на соответствие условию N > M > V
+        private bool CheckInequality()
+        {
+            if (!CheckVal())
+                return false;
+            if (N < M || N < V || M < V)
+            {
+                MessageBox.Show("Значения не соответствуют условию 'Размер пустыни > Размер бака > Размер канистры'!");
+                return false;
+            }
+            return true;
+        }
+
+        void Task()
+        {
+            if (!CheckInequality())
+                MessageBox.Show("Исправьте необходимые значения!");
+            else
+            {
+                string result_str = "";
+                if ((M + 2 * V) >= N)
+                    result_str = "Количество использованного топлива: " + N + "\r\nМашина пересекла пустыню.";
+                else
+                {
+                    double need_track = (N - (M + 2 * V));
+                    double track = 0.0;
+                    double cnt_fuel = 0.0;
+                    double[] val_of_departure = new double[20];
+                    for (int i = 1; track < need_track; i++)
+                    {
+                        track = (M / 2 + i * V / 2);
+                        cnt_fuel += (M + i * V + V);
+                        val_of_departure[i] = cnt_fuel;
+                        result_str += overcome + track.ToString() + leave_canister + "\r\n" + return_to_base + "\r\n";                        
+                        for (int j = 1; j < i; j ++)
+                        {
+                            result_str += "Машина повторяет предыдущие поездки\r\n";
+                            cnt_fuel += val_of_departure[j];
+                        }
+                    }
+                    cnt_fuel += N - track;
+                    result_str += overcome + (N - track).ToString() + "\r\n" + "Количество использованного топлива: " + cnt_fuel.ToString() + "\r\nМашина пересекла пустыню.";
+                }
+                tbResult.Text = result_str;
+            }
         }
     }
 }
